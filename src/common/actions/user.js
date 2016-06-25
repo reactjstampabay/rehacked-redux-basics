70f490ecab676fc6e7b12b2598166c920da7c6c3
Eric Nograles <ENograles@352inc.com>
2016-06-25 15:17:19 -0400

    Initial install of Redux and establishment of actions to be performed by the UI

---
+++ src/common/actions/user.js
@@ -0,0 +1,35 @@
+import login from '../services/user';
+
+export const REQUEST_LOGIN = 'REQUEST_LOGIN';
+export const RECEIVE_LOGIN = 'RECEIVE_LOGIN';
+export const LOGOUT = 'LOGOUT';
+
+export function requestLogin() {
+  return {
+    type: REQUEST_LOGIN
+  };
+}
+
+function receiveLogin(email, response) {
+  return {
+    type: RECEIVE_LOGIN,
+    email: email,
+    profile: response.data,
+    error: response.error
+  };
+}
+
+export function initiateLogin(email, password) {
+  return dispatch => {
+    dispatch(requestLogin());
+    return login(email, password)
+      .then(json => dispatch(receiveLogin(email, {data: json})))
+      .catch(error => dispatch(receiveLogin(email, {error: error})));
+  }
+}
+
+export function logout() {
+  return {
+    type: LOGOUT
+  };
+}
