f434de43bf2fbd4368737dc0279144c0c7faffd5
johnrhampton <johnrhampton@gmail.com>
2016-07-07 15:19:30 -0400

    updateLoginField, pulling out functions

--- src/common/reducers/user.js
+++ src/common/reducers/user.js
@@ -1,30 +1,56 @@
 import {
+  LOGOUT,
   REQUEST_LOGIN,
   RECEIVE_LOGIN,
-  LOGOUT
+  UPDATE_LOGIN_FIELD
 } from '../actions/user';
 
-export function user(state = {}, action) {
+/**
+ * export user reducer
+ */
+export const user = (state = {}, action) => {
   switch (action.type) {
-    case REQUEST_LOGIN:
-      return Object.assign({}, state, {
-        status: 'authenticating'
-      });
-      break;
-    case RECEIVE_LOGIN:
-      return Object.assign({}, state, {
-        profile: action.profile,
-        error: action.error,
-        status: action.error ? 'unauthorized' : 'authorized'
-      });
-      break;
     case LOGOUT:
-      return Object.assign({}, state,  {
-        status: 'logged_out',
-        profile: null
-      });
-      break;
+      return logout(state, action);
+    case REQUEST_LOGIN:
+      return requestLogin(state, action);
+    case RECEIVE_LOGIN:
+      return receiveLogin(state, action);
+    case UPDATE_LOGIN_FIELD:
+      return updateLoginField(state, action);
     default:
       return state;
   }
+};
+
+function logout(state, action) {
+  return Object.assign({}, state, {
+    status: 'logged_out',
+    profile: null
+  });
+}
+
+function receiveLogin(state, action) {
+  return Object.assign({}, state, {
+    profile: action.profile,
+    error: action.error,
+    status: action.error ? 'unauthorized' : 'authorized'
+  });
+}
+
+function requestLogin(state, action) {
+  return Object.assign({}, state, {
+    status: 'authenticating'
+  });
+}
+
+function updateLoginField(state, action) {
+  var login_profile = Object.assign({}, state.login_profile);
+  login_profile[action.key] = action.value;
+
+  return Object.assign({}, state, {
+    type: UPDATE_LOGIN_FIELD,
+    error: null,
+    login_profile: login_profile
+  });
 }
