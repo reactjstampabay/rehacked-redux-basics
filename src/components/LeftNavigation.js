b71a03f8aac842a6804e7b11aecb83b26765b2a3
Eric Nograles <ENograles@352inc.com>
2016-07-08 12:14:44 -0400

    swapping out `var` with `let` to be more consistent with ES6

--- src/components/LeftNavigation.js
+++ src/components/LeftNavigation.js
@@ -4,7 +4,7 @@ import NavigationMenu from './NavigationMenu';
 
 export default class LeftNavigation extends Component {
   render() {
-    var {handleLogout, user} = this.props;
+    let {handleLogout, user} = this.props;
     return (
       <div className="demo-drawer mdl-layout__drawer mdl-color--blue-grey-900 mdl-color-text--blue-grey-50">
         <Avatar user={user} handleLogout={handleLogout} />
