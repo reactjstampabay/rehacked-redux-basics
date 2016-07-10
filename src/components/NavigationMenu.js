b639815a64a7d48ff47b0daf06946fc8a7e16a57
johnrhampton <johnrhampton@gmail.com>
2016-07-09 20:35:15 -0400

    NavigationMenu simple function

--- src/components/NavigationMenu.js
+++ src/components/NavigationMenu.js
@@ -1,12 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-export default class NavigationMenu extends Component {
-  render() {
-    return (
-      <nav className="demo-navigation mdl-navigation mdl-color--blue-grey-800">
-        <a className="mdl-navigation__link" href="#/dashboard"><i className="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i>Home</a>
-        <a className="mdl-navigation__link" href="#/dashboard"><i className="mdl-color-text--blue-grey-400 material-icons" role="presentation">inbox</i>Inbox</a>
-      </nav>
-    );
-  }
-}
+export default () => {
+  return (
+    <nav className="demo-navigation mdl-navigation mdl-color--blue-grey-800">
+      <a className="mdl-navigation__link" href="#/dashboard"><i className="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i>Home</a>
+      <a className="mdl-navigation__link" href="#/dashboard"><i className="mdl-color-text--blue-grey-400 material-icons" role="presentation">inbox</i>Inbox</a>
+    </nav>
+  );
+};
