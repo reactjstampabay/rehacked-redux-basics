1f4cfc23726e1aa2d5c8e11351d2585df4357fd2
johnrhampton <johnrhampton@gmail.com>
2016-07-12 09:31:18 -0400

    moving login state to StartScreen container

--- src/containers/StartScreen/index.js
+++ src/containers/StartScreen/index.js
@@ -1,7 +1,7 @@
 import React, {Component, PropTypes} from 'react';
 import { connect } from 'react-redux'; 
 import Login from '../../components/Login';
-import {initiateLogin, updateLoginField} from '../../common/actions/user';
+import {initiateLogin} from '../../common/actions/user';
 import {hashHistory} from 'react-router';
 
 class StartScreen extends Component {
@@ -27,24 +27,24 @@ class StartScreen extends Component {
   }
 
   _handleFieldChange(field, event) {
-    this.props.dispatch(updateLoginField(field, event.target.value));
+    let newState = Object.assign({}, this.state);
+    newState[field] = event.target.value;
+    this.setState(newState);
   }
 
   _handleLogin() {
     let errors = [];
-    let user = this.props.user;
-    
-    if (!user.email) {
+    if (!this.state.email) {
       errors.push('You must specify an email');
     }
-    if (!user.password) {
+    if (!this.state.password) {
       errors.push('You must specify a password');
     }
 
     if (errors.length > 0) {
       this._showSnackBar(errors.join('.'));
     } else {
-      this.props.dispatch(initiateLogin(user.email, user.password));
+      this.props.dispatch(initiateLogin(this.state.email, this.state.password));
     }
   }
 
@@ -58,13 +58,12 @@ class StartScreen extends Component {
   }
 
   render() {
-    let {user} = this.props;
     return (
-      <Login email={user.email}
-             password={user.password}
+      <Login email={this.state.email}
+             password={this.state.password}
              handleFieldChange={this._handleFieldChange}
              handleLogin={this._handleLogin}
-             loading={user.status === 'authenticating'}/>
+             loading={this.props.status === 'authenticating'}/>
     );
   }
 }
